293c6c00d4e0137ad7025ff2002c89f27458004c
TimJonsson1 <44998865+TimJonsson1@users.noreply.github.com>
2019-12-02 18:10:41 +0100

    "booking 13th try"

--- src/com/company/Booking.java
+++ src/com/company/Booking.java
@@ -1,4 +1,23 @@
 package com.company;
 
 public class Booking {
+
+    private int bookingId;
+    private double TotalPrice;
+
+    public Booking(int bookingId, double totalPrice) {
+        this.bookingId = bookingId;
+        TotalPrice = totalPrice;
+    }
+
+    public int getBookingId(){
+        return bookingId;
+    }
+
+    @Override
+    public String toString() {
+        return "Room nr:" + bookingId + "\n" +
+                "TotalPrice: " + TotalPrice + "kr\n" +
+                "-----------------------------------";
+
 }
