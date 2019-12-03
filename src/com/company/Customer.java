1ee1f9b8a7385f55b84fe4ecc7b5253a7bd9237b
ahmadelj <ahmad.eljachi98@gmail.com>
2019-12-03 12:11:53 +0100

    fixed customer

--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -1,4 +1,26 @@
 package com.company;
 
 public class Customer {
+    private String socialSecurityNumber;
+    private String name;
+    private String address;
+    private String telephoneNumber;
+
+    public Customer(String socialSecurityNumber, String name, String address, String telephoneNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.name = name;
+        this.address = address;
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "socialSecurityNumber='" + socialSecurityNumber + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", telephoneNumber='" + telephoneNumber + '\'' +
+                '}';
+    }
 }
+
