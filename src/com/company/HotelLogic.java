7fe4d520388f24742908d2fbab7f1945bddec272
Ecceptz <Jesperekblad97@hotmail.com>
2019-12-04 16:40:20 +0100

    Added Room class + addRoom method

--- src/com/company/HotelLogic.java
+++ src/com/company/HotelLogic.java
@@ -1,10 +1,44 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HotelLogic {
 
-public ArrayList<Room> roomsArr = new ArrayList<>();
+    ArrayList<Room> rooms = new ArrayList<>();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public ArrayList<Room> addRoom (ArrayList rooms)   {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Roomnumber: ");
+        int RoomNumber = input.nextInt();
+        System.out.print("Beds:  ");
+        int NumberOfBeds = input.nextInt();
+        System.out.print("Balcony ");
+        boolean HasBalcony = input.nextBoolean();
+        System.out.print("Price ");
+        double PricePerNight = input.nextDouble();
+        System.out.print("Booked ");
+        boolean isBooked = input.nextBoolean();
+
+
+        Room room = new Room(RoomNumber, NumberOfBeds, HasBalcony, PricePerNight, isBooked);
+        rooms.add(room);
+
+        return rooms;
+    }
 
 
 }
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,4 +1,70 @@
 package com.company;
 
 public class Room {
+    private int roomNumber;
+    private int numberOfBeds;
+    private boolean hasBalcony;
+    private double pricePerNight;
+    private boolean isBooked;
+
+    public Room(int roomNumber, int numberOfBeds, boolean hasBalcony, double pricePerNight, boolean isBooked)   {
+        this.setRoomNumber(roomNumber);
+        this.setNumberOfBeds(numberOfBeds);
+        this.setHasBalcony(hasBalcony);
+        this.setPricePerNight(pricePerNight);
+        this.setisBooked(isBooked);
+    }
+
+    public void setRoomNumber (int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public int getRoomNumber () {
+        return roomNumber;
+    }
+
+    public void setNumberOfBeds (int numberOfBeds)  {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public int getNumberOfBeds ()   {
+        return numberOfBeds;
+    }
+
+    public void setHasBalcony (boolean hasBalcony)  {
+        this.hasBalcony = hasBalcony;
+    }
+
+    public boolean getHasBalcony () {
+        return hasBalcony;
+    }
+
+    public void setPricePerNight (double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public double getPricePerNight ()   {
+        return pricePerNight;
+    }
+
+    public void setisBooked (boolean isBooked)  {
+        this.isBooked = isBooked;
+    }
+
+    public boolean getisBooked ()   {
+        return isBooked;
+    }
+
+    @Override
+    public String toString() {
+        return "Room " +
+                "\nRoom number: " + roomNumber +
+                "\nNumber of beds: " + numberOfBeds +
+                "\nBalcony: " + hasBalcony +
+                "\nPrice per night: " + pricePerNight +
+                "\nRoom booked: " + isBooked +
+                "\n------------------------------------- \n";
+
+    }
 }
+
